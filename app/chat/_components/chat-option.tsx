eb0bfcab656b648744ccb2e4f99aabf00383bf5e
davitti <abuadonald@gmail.com>
2024-02-05 01:29:49 +0100

    Enhanced ChatOption component in app/chat/_components/chat-option.tsx

--- app/chat/_components/chat-option.tsx
+++ app/chat/_components/chat-option.tsx
@@ -1,4 +1,5 @@
 "use client";
+import pipeline from "@/lib/pipeline";
 import { cn } from "@/lib/utils";
 import { GlobalStateContext } from "@/state/global.state";
 import React, { ReactNode, useContext } from "react";
@@ -10,15 +11,20 @@ interface ChatOptionProp {
 export default function ChatOption({ children, id }: ChatOptionProp) {
 	const {
 		globalRuntimeState: { chatOption },
+		globalDispatch,
 	} = useContext(GlobalStateContext);
 	return (
-		<div
+		<button
 			className={cn(
-				"px-4 py-2 rounded-[18px] outline outline-1 outline-pallete-electric-indigo text-pallete-electric-indigo flex-grow flex-shrink-0 basis-[45%]",
+				"px-4 py-2 rounded-[18px] outline outline-1 outline-pallete-electric-indigo text-pallete-electric-indigo flex-grow flex-shrink-0 basis-[45%] active:scale-95 transition",
 				chatOption === id && "bg-pallete-electric-indigo text-white",
 			)}
+			onClick={() => {
+				globalDispatch({ type: "chat_option", id });
+				pipeline(globalDispatch);
+			}}
 		>
 			{children}
-		</div>
+		</button>
 	);
 }
