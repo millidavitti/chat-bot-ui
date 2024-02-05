3580e08be95782ac9a2571bce51a3566220220ea
davitti <abuadonald@gmail.com>
2024-02-05 01:22:31 +0100

    Added remotePatterns for images in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "source.unsplash.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "plus.unsplash.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
