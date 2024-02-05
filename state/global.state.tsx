b022c87dd9f8867f1a8e2f37ea63fe6b75549db9
davitti <abuadonald@gmail.com>
2024-02-05 01:40:26 +0100

    Enhanced global state management in state/global.state.tsx

--- state/global.state.tsx
+++ state/global.state.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { v4 as uuidv4 } from "uuid";
 import { Dispatch, createContext, useReducer } from "react";
 import { LocalStorageManager } from "./localStorageManager";
+import {
+	ChatBotResponseProp,
+	UserChatRequestProp,
+} from "@/app/chat/_components/chat-bot";
 
 export interface GlobalState {
 	isConversationSheetOpen: boolean;
 	currentConversation: string;
 	chatOption: string;
+	chatHistory: Array<ChatBotResponseProp | UserChatRequestProp>;
+	isGeneratingResponse: boolean;
 }
 
 const globalState = new LocalStorageManager<GlobalState>("global");
@@ -14,6 +21,14 @@ globalState.initState({
 	isConversationSheetOpen: false,
 	currentConversation: "",
 	chatOption: "A",
+	chatHistory: [
+		{
+			id: "1",
+			response: "What can I help you with?",
+			type: "ChatBotResponseProp",
+		},
+	],
+	isGeneratingResponse: false,
 });
 
 export function globalStateReducer(
@@ -22,16 +37,74 @@ export function globalStateReducer(
 ): GlobalState {
 	switch (action.type) {
 		case "toggle_conversation_sheet": {
-			return globalState.setState({
-				...state,
-				isConversationSheetOpen: !state.isConversationSheetOpen,
-			});
+			return globalState
+				.setState({
+					...state,
+					isConversationSheetOpen: !state.isConversationSheetOpen,
+				})
+				.getState();
 		}
 		case "select_conversation": {
-			return globalState.setState({
-				...state,
-				currentConversation: action.id,
-			});
+			return globalState
+				.setState({
+					...state,
+					currentConversation: action.id,
+				})
+				.getState();
+		}
+		case "chat_option": {
+			if (action.id === state.chatOption)
+				return globalState
+					.setState({
+						...state,
+						chatHistory: [
+							...state.chatHistory,
+							{
+								id: uuidv4(),
+								request: "Option A",
+								type: "UserChatRequestProp",
+							},
+						],
+					})
+					.getState();
+		}
+		case "is_generating_response": {
+			return globalState
+				.setState({
+					...state,
+					isGeneratingResponse: action.status,
+				})
+				.getState();
+		}
+		case "chat_response": {
+			return globalState
+				.setState({
+					...state,
+					chatHistory: [
+						...state.chatHistory,
+						{
+							id: uuidv4(),
+							response: action.response,
+							type: "ChatBotResponseProp",
+						},
+					],
+				})
+				.getState();
+		}
+		case "chat_request": {
+			return globalState
+				.setState({
+					...state,
+					chatHistory: [
+						...state.chatHistory,
+						{
+							id: uuidv4(),
+							request: action.request,
+							type: "UserChatRequestProp",
+						},
+					],
+				})
+				.getState();
 		}
 
 		default:
@@ -49,6 +122,14 @@ export const GlobalStateContext = createContext<GlobalReducer>({
 		isConversationSheetOpen: false,
 		currentConversation: "",
 		chatOption: "A",
+		chatHistory: [
+			{
+				id: "1",
+				response: "What can I help you with?",
+				type: "ChatBotResponseProp",
+			},
+		],
+		isGeneratingResponse: false,
 	},
 	globalDispatch() {},
 });
